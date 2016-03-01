9b9e2c596f6e46ff162c0e15d29bb643c2ee94a6
Jimmy Brown <jabro4@gmail.com>
2016-03-01 15:03:52 -0800

    Getting error in AppDelegate when trying to build app.

--- MyHood/AddPostVC.swift
+++ MyHood/AddPostVC.swift
@@ -8,28 +8,41 @@
 
 import UIKit
 
-class AddPostVC: UIViewController {
+class AddPostVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var descField: UITextField!
     @IBOutlet weak var titleField: UITextField!
     @IBOutlet weak var postImg: UIImageView!
+    
+    var imagePicker: UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         postImg.layer.cornerRadius = postImg.frame.size.width / 2
         postImg.clipsToBounds = true
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
     }
 
     @IBAction func addPicBtnPressed(sender: UIButton) {
         sender.setTitle("", forState: .Normal)
+        presentViewController(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func makePostBtnPressed(sender: AnyObject) {
-        
+        if let title = titleField.text, let desc = descField.text, let img = postImg.image {
+            let post = Post(imagePath: "", title: title, description: desc)
+            DataService.instance.addPost(post)
+        }
     }
     
     @IBAction func cancelBtnPressed(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        postImg.image = image
     }
 }
